3fc96cec5d306df581b4e239f0c42f04c8795bbf
xiaotiandada <952822399@qq.com>
2019-04-30 23:28:02 +0800

    test
    test

--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import VueMeta from "vue-meta";
 
+Vue.use(VueMeta);
 Vue.use(Router);
 
 export default new Router({
