2d6712bb58a7865b13ac8d49c965295c43657664
xiaotiandada <952822399@qq.com>
2019-04-30 16:27:15 +0800

    tese
    tese

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,39 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
   name: 'app',
   components: {
     HelloWorld
-  }
+  },
+  created(){
+    this.getdata()
+  },
+  methods: {
+    getdata(){
+      axios.get('https://www.easy-mock.com/mock/5cc804f971639a150fce2fcd/api/')
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    }
+  },
+    head: {
+    title: {
+      inner: 'It will be a pleasure'
+    },
+    // Meta tags
+    meta: [
+      // Facebook / Open Graph
+      { property: 'fb:app_id', content: '123456789' },
+      { property: 'og:title', content: 'Content Title' },
+      { property: 'og:image', content: 'http://p98.pstatp.com/large/pgc-image/15331766316513c010b5e01' },
+    ],
+    }
 }
 </script>
 
