af505275125b1a20fbe98a5fa91b3f8ac9644082
xiaotiandada <952822399@qq.com>
2019-05-04 01:54:49 +0800

    test
    test

---
+++ test/src/entry-client.js
@@ -0,0 +1,14 @@
+import { loadAsyncComponents } from "@akryum/vue-cli-plugin-ssr/client";
+
+import { createApp } from "./main";
+
+createApp({
+  async beforeApp({ router }) {
+    await loadAsyncComponents({ router });
+  },
+
+  afterApp({ app, store }) {
+    store.replaceState(window.__INITIAL_STATE__);
+    app.$mount("#app");
+  }
+});
--- test/src/main.js
+++ test/src/main.js
@@ -1,18 +1,35 @@
 import Vue from "vue";
 import App from "./App.vue";
-
 import { createRouter } from "./router";
 import { createStore } from "./store";
 
 Vue.config.productionTip = false;
 
-export async function createApp() {
+export async function createApp({
+  beforeApp = () => {},
+  afterApp = () => {}
+} = {}) {
   const router = createRouter();
   const store = createStore();
 
-  return new Vue({
+  await beforeApp({
+    router,
+    store
+  });
+
+  const app = new Vue({
     router,
     store,
     render: h => h(App)
-  }).$mount("#app");
+  });
+
+  const result = {
+    app,
+    router,
+    store
+  };
+
+  await afterApp(result);
+
+  return result;
 }
